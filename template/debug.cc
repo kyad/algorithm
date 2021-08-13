8a466320cf6f2fc39fc33d5e5641035f5d5b240f
kyad <53517611+kyad@users.noreply.github.com>
2021-08-13 15:54:59 +0900

    Add debug code

--- template/debug.cc
+++ template/debug.cc
@@ -6,3 +6,15 @@ template<class T> ostream& operator<<(ostream& os, const vector<T> &v) {
   }
   return os;
 }
+
+// Print 2d-matrix
+template<class T> ostream& operator<<(ostream& os, const vector<vector<T> > &v) {
+  for (size_t i = 0; i < v.size(); i++) {
+    os << (i == 0 ? "[[" : " [");
+    for (size_t j = 0; j < v[i].size(); j++) {
+      os << v[i][j] << (j == v[i].size() - 1 ? ']' : ' ');
+    }
+    os << (i == v.size() - 1 ? ']' : '\n');
+  }
+  return os;
+}
