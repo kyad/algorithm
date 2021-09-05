17358baf30ce32cffad73d0e9286c96145b01403
kyad <53517611+kyad@users.noreply.github.com>
2021-09-05 17:55:19 +0900

    Add debug code

--- template/debug.cc
+++ template/debug.cc
@@ -1,4 +1,4 @@
-// Print vector
+// vector
 template<class T> ostream& operator<<(ostream& os, const vector<T> &v) {
   os << '[';
   for (size_t i = 0; i < v.size(); i++) {
@@ -7,7 +7,7 @@ template<class T> ostream& operator<<(ostream& os, const vector<T> &v) {
   return os;
 }
 
-// Print 2d-matrix
+// 2d-matrix
 template<class T> ostream& operator<<(ostream& os, const vector<vector<T> > &v) {
   for (size_t i = 0; i < v.size(); i++) {
     os << (i == 0 ? "[[" : " [");
@@ -15,6 +15,28 @@ template<class T> ostream& operator<<(ostream& os, const vector<vector<T> > &v) 
       os << v[i][j] << (j == v[i].size() - 1 ? ']' : ' ');
     }
     os << (i == v.size() - 1 ? ']' : '\n');
+  }
+  return os;
+}
+
+// multiset
+template<class T> ostream& operator<<(ostream& os, const multiset<T> &ms) {
+  for (typename multiset<T>::iterator it = ms.begin(); it != ms.end(); it++) {
+    os << *it;
+    if (it != prev(ms.end())) {
+      os << ' ';
+    }
+  }
+  return os;
+}
+
+// deque
+template<class T> ostream& operator<<(ostream& os, const deque<T> &dq) {
+  for (auto it = dq.begin(); it != dq.end(); it++) {
+    os << *it;
+    if (it != prev(dq.end())) {
+      os << ' ';
+    }
   }
   return os;
 }
