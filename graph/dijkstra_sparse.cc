6a3aaf1dc5d85d9ab76900050ef28b4094f7cc3a
kyad <53517611+kyad@users.noreply.github.com>
2021-09-23 23:00:09 +0900

    Update Dijkstra's algorithm

--- graph/dijkstra_sparse.cc
+++ graph/dijkstra_sparse.cc
@@ -1,78 +1,102 @@
-// https://atcoder.jp/contests/abc213/tasks/abc213_e
+// https://atcoder.jp/contests/joi2016yo/tasks/joi2016yo_e
 // https://github.com/drken1215/book_algorithm_solution/blob/master/codes/chap14/code_14_4.cpp
-// https://blog.hamayanhamayan.com/entry/2021/08/09/010308
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
-void print_dist(vector<int> dist, int W) {
-  const int INF = 1000000000;
-  int H = dist.size() / W;
-  for (int y = 0; y < H; y++) {
-    for (int x = 0; x < W; x++) {
-      int idx = y + x * H;
-      int d = dist[idx];
-      if (d >= INF) {
-        printf("inf");
-      } else {
-        printf("%03d", d);
+int main() {
+  int N, M, K, S;
+  cin >> N >> M >> K >> S;
+  int P, Q;
+  cin >> P >> Q;
+  vector<int> C(K);
+  for (int k = 0; k < K; k++) {
+    cin >> C.at(k);
+    C.at(k)--;
+  }
+  vector<int> A(M), B(M);
+  for (int m = 0; m < M; m++) {
+    cin >> A.at(m) >> B.at(m);
+    A.at(m)--;
+    B.at(m)--;
+  }
+  vector<vector<int> > graph1(N);
+  for (int m = 0; m < M; m++) {
+    graph1.at(A.at(m)).push_back(B.at(m));
+    graph1.at(B.at(m)).push_back(A.at(m));
+  }
+  // BFS
+  queue<int> todo;
+  vector<int> dist1(N, -1);
+  for (int k = 0; k < K; k++) {
+    todo.push(C.at(k));
+    dist1.at(C.at(k)) = 0;
+  }
+  while (!todo.empty()) {
+    int v = todo.front();
+    todo.pop();
+    for (size_t i = 0; i < graph1.at(v).size(); i++) {
+      int nv = graph1.at(v).at(i);
+      if (dist1.at(nv) >= 0) {
+        continue;
       }
-      if (x == W - 1) {
-        putchar('\n');
-      } else {
-        putchar(' ');
-      }
+      dist1.at(nv) = dist1.at(v) + 1;
+      todo.push(nv);
     }
   }
-}
-
-int main() {
-  const int INF = 1000000000;
-  int H, W;
-  cin >> H >> W;
-  vector<string> S(H);
-  for (int h = 0; h < H; h++) {
-    cin >> S[h];
+  vector<vector<pair<int, int> > > graph2(N);
+  for (int m = 0; m < M; m++) {
+    int a = A.at(m);
+    int b = B.at(m);
+    // a --> b
+    if (b == 0) {
+    } else if (b == N - 1) {
+      graph2.at(a).push_back(make_pair(b, 0));
+    } else if (dist1.at(b) > S) {
+      // danger
+      graph2.at(a).push_back(make_pair(b, P));
+    } else if (dist1.at(b) > 0) {
+      // safe
+      graph2.at(a).push_back(make_pair(b, Q));
+    }
+    // b --> a
+    if (a == 0) {
+    } else if (a == N - 1) {
+      graph2.at(b).push_back(make_pair(a, 0));
+    } else if (dist1.at(a) > S) {
+      // danger
+      graph2.at(b).push_back(make_pair(a, P));
+    } else if (dist1.at(a) > 0) {
+      // safe
+      graph2.at(b).push_back(make_pair(a, Q));
+    }
   }
   // Dijkstra's algorithm for sparse graph (O|E|log|V|)
-  vector<int> dist(H * W, INF);
-  dist[0 * W + 0] = 0;
-  priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
-  pq.push(make_pair(dist[0 * W + 0], 0 * W + 0));
+  const long long INF = (long long)100000 * 200000 + 1;
+  vector<long long> dist2(N, INF);
+  dist2.at(0) = 0;
+  priority_queue<pair<long long, int>, vector<pair<long long, int> >, greater<pair<long long, int> > > pq;
+  pq.push(make_pair(0, 0));
   while (!pq.empty()) {
-    int min_dist_idx = pq.top().second;
-    int min_dist = pq.top().first;
+    long long dist_min = pq.top().first;
+    int dist_min_idx = pq.top().second;
     pq.pop();
-    if (min_dist > dist[min_dist_idx]) {
+    if (dist_min > dist2.at(dist_min_idx)) {
+      // garbage
       continue;
     }
-    int y = min_dist_idx / W;
-    int x = min_dist_idx % W;
-    const int dy[4] = {0, -1, 0, 1};
-    const int dx[4] = {-1, 0, 1, 0};
-    for (int i = 0; i < 4; i++) {
-      int yy = y + dy[i];
-      int xx = x + dx[i];
-      if (0 <= xx && xx < W && 0 <= yy && yy < H) {
-        if (S[yy][xx] == '.') {
-          if (dist[yy * W + xx] > min_dist) {
-            dist[yy * W + xx] = min_dist;
-            pq.push(make_pair(dist[yy * W + xx], yy * W + xx));
-          }
-        }
-      }
-    }
-    for (int yy = y - 2; yy <= y + 2; yy++) {
-      for (int xx = x - 2; xx <= x + 2; xx++) {
-        if (0 <= xx && xx < W && 0 <= yy && yy < H && (abs(x - xx) + abs(y - yy) < 4)) {
-          if (dist[yy * W + xx] > min_dist + 1) {
-            dist[yy * W + xx] = min_dist + 1;
-            pq.push(make_pair(dist[yy * W + xx], yy * W + xx));
-          }
-        }
+    vector<pair<int, int> > edges = graph2.at(dist_min_idx);
+    for (size_t i = 0; i < edges.size(); i++) {
+      int v = edges.at(i).first;
+      int cost = edges.at(i).second;
+      if (dist2.at(v) > dist_min + cost) {
+        dist2.at(v) = dist_min + cost;
+        pq.push(make_pair(dist2.at(v), v));
       }
     }
   }
-  cout << dist[(H - 1) * W + (W - 1)] << endl;
+  cout << dist2.at(N - 1) << endl;
   return 0;
 }
