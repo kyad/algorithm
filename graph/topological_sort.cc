5a045dea9db836c60b97b04999d9ff393255863c
kyad <53517611+kyad@users.noreply.github.com>
2021-10-21 23:24:47 +0900

    Add topological sort

---
+++ graph/topological_sort.cc
@@ -0,0 +1,50 @@
+// https://atcoder.jp/contests/abc223/tasks/abc223_d
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<vector<int> > graph(N);
+  vector<int> indeg(N, 0);
+  for (int m = 0; m < M; m++) {
+    int A, B;
+    cin >> A >> B;
+    A--;
+    B--;
+    indeg.at(B)++;
+    graph[A].push_back(B);
+  }
+  // Topological sort
+  priority_queue<int, vector<int>, greater<int> > todo;  // Lexical order
+  for (int n = 0; n < N; n++) {
+    if (indeg.at(n) == 0) {
+      todo.push(n);
+    }
+  }
+  vector<int> ans;
+  while (!todo.empty()) {
+    int u = todo.top();
+    todo.pop();
+    ans.push_back(u);
+    for (size_t i = 0; i < graph.at(u).size(); i++) {
+      int v = graph.at(u).at(i);
+      indeg.at(v)--;
+      if (indeg.at(v) == 0) {
+        todo.push(v);
+      }
+    }
+  }
+  // Check if graph have loop
+  if (ans.size() != (size_t)N) {
+    cout << -1 << endl;
+    return 0;
+  }
+  for (size_t i = 0; i < ans.size(); i++) {
+    cout << (ans[i] + 1) << (i == (ans.size() - 1) ? '\n' : ' ');
+  }
+  return 0;
+}
