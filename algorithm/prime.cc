c08451aa5415a75724664c4bc091014c5e060519
kyad <53517611+kyad@users.noreply.github.com>
2022-02-20 01:43:35 +0900

    Improve prime

--- algorithm/prime.cc
+++ algorithm/prime.cc
@@ -1,12 +1,32 @@
 // https://atcoder.jp/contests/abc239/tasks/abc239_d
 
-#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// int(sqrt(n)) for n >= 0
+const int INF = 100000;
+int int_sqrt(int n) {
+  if (n == 0) {
+    return 0;
+  }    
+  int left = 0;
+  int right = INF;
+  while (right - left > 1) {
+    int mid = (left + right) / 2;
+    long long y = (long long)mid * mid - n;
+    if (y <= 0) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
+
+// O(sqrt(n))
 vector<int> prime_factorization(int n) {
-  int max = (int)sqrt(n);
+  int max = int_sqrt(n);
   vector<int> ans;
   for (int m = 2; m <= max; m++) {
     while (n % m == 0) {
@@ -20,6 +40,7 @@ vector<int> prime_factorization(int n) {
   return ans;
 }
 
+// O(sqrt(n))
 bool is_prime(int n) {
   vector<int> v = prime_factorization(n);
   return (v.size() == 1);
