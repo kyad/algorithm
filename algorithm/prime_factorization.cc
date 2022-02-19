c6c7cf10978a3b647d86771534f26055245364b7
kyad <53517611+kyad@users.noreply.github.com>
2022-02-20 01:32:51 +0900

    Improve prime factorization

--- algorithm/prime_factorization.cc
+++ algorithm/prime_factorization.cc
@@ -1,12 +1,32 @@
 // https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
 
-#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// int(sqrt(n)) for n >= 0
+const int INF = 100000;
+int int_sqrt(int n) {
+  if (n == 0) {
+    return 0;
+  }    
+  int left = 0;
+  int right = INF;
+  while (right - left > 1) {
+    int mid = (left + right) / 2;
+    long long y = (long long)mid * mid - n;
+    if (y <= 0) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
+
+// O(sqrt(n))
 vector<int> prime_factorization(int n) {
-  int max = (int)sqrt(n);
+  int max = int_sqrt(n);
   vector<int> ans;
   for (int m = 2; m <= max; m++) {
     while (n % m == 0) {
