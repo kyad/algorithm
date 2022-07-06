a65816c833f0a9fc0eea328b9b52ecab6332e394
kyad <53517611+kyad@users.noreply.github.com>
2022-07-07 00:28:30 +0900

    Add lazy_segtree print

--- template/debug_acl.cc
+++ template/debug_acl.cc
@@ -5,6 +5,22 @@ ostream& operator<<(ostream& os, const mint &v) {
 }
 
 // lazy_segtree
+void print_segtree(lazy_segtree<S, op, e, F, mapping, composition, id> &seg, size_t N) {
+  int MOD = -1;
+  cout << '[';
+  for (size_t i = 0; i < N; i++) {
+    cout << seg.get(i);
+    if (i == N - 1) {
+      continue;
+    } else if (MOD > 0 && (int)i % MOD == (MOD - 1)) {
+      cout << '\n';
+    } else {
+      cout << ' ';
+    }
+  }
+  cout << ']' << endl;
+}
+
 ostream& operator<<(ostream& os, lazy_segtree<S, op, e, F, mapping, composition, id> seg) {
   const int N = 1;
   int MOD = -1;
