a52a3ba53fb73aa7e73da9f55130e3ece2619392
kyad <53517611+kyad@users.noreply.github.com>
2022-08-06 17:31:20 +0900

    Update binomial coefficients

--- algorithm/binomial_coefficients.cc
+++ algorithm/binomial_coefficients.cc
@@ -1,11 +1,12 @@
-// https://atcoder.jp/contests/abc034/tasks/abc034_c
+// https://atcoder.jp/contests/abc262/tasks/abc262_e
 // https://drken1215.hatenablog.com/entry/2018/06/08/210000
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 const int MAX = 510000;  // n (<= 10^7)
-const int MOD = 1000000007;  // p (prime, > n)
+const int MOD = 998244353;  // p (prime, > n)
 
 long long fac[MAX];  // a! mod p
 long long finv[MAX];  // (a!)^{-1} mod p
@@ -35,9 +36,35 @@ long long COM(int n, int k) {
 }
 
 int main() {
-  int W, H;
-  cin >> W >> H;
   COMinit();
-  cout << COM(W + H - 2, W - 1) << endl;
+  int N, M, K;
+  cin >> N >> M >> K;
+  vector<int> deg(N, 0);
+  for (int m = 0; m < M; m++) {
+    int U, V;
+    cin >> U >> V;
+    U--; V--;
+    deg[U]++;
+    deg[V]++;
+  }
+  int O = 0;  // 次数が奇数の頂点数
+  int E = 0;  // 次数が偶数の頂点数
+  for (int n = 0; n < N; n++) {
+    if (deg[n] % 2) {
+      O++;
+    } else {
+      E++;
+    }
+  }
+  long long ans = 0;
+  for (int o = 0; o <= K; o += 2) {
+    int e = K - o;
+    if (o > O || e > E) {
+      continue;
+    }
+    ans += (COM(O, o) * COM(E, e)) % MOD;
+    ans %= MOD;
+  }
+  cout << ans << endl;
   return 0;
 }
