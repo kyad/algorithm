27fb84f652f7bb01d140c17d445bcf5a3a32b529
kyad <53517611+kyad@users.noreply.github.com>
2022-08-11 22:59:58 +0900

    Make debug code shorter

--- template/debug.cc
+++ template/debug.cc
@@ -6,7 +6,9 @@
 #include <tuple>
 using namespace std;
 
-// debug print
+// debug
+#include <algorithm>
+#include <vector>
 #define print1(a) std::cout << #a << '=' << a << std::endl;
 #define print2(a, b) print1(a); print1(b);
 #define print3(a, b, c) print1(a); print2(b, c);
@@ -17,154 +19,34 @@ using namespace std;
 #define print8(a, b, c, d, e, f, g, h) print1(a); print7(b, c, d, e, f, g, h);
 #define print_select(a, b, c, d, e, f, g, h, i, ...) i
 #define print(...) print_select(__VA_ARGS__, print8, print7, print6, print5, print4, print3, print2, print1)(__VA_ARGS__)
-
-// pair
-template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1, T2> &p) {
-  os << "(" << p.first << ", " << p.second << ")";
-  return os;
-}
-
-// vector
-template<class T> ostream& operator<<(ostream& os, const vector<T> &v) {
-  size_t N = v.size();
-  int MOD = -1;
-  os << '[';
-  for (size_t i = 0; i < N; i++) {
-    os << v[i];
-    if (i == N - 1) {
-      continue;
-    } else if (MOD > 0 && i % MOD == (MOD - 1)) {
-      os << '\n';
-    } else {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// set
+template<class T> ostream& operator<<(ostream& os, const vector<T> &v) { int MOD = -1; size_t N = v.size(); os << '[';
+  for (size_t i = 0; i < N; i++) { os << v[i];
+    if (i == N - 1) { continue; }
+    else if (MOD > 0 && i % MOD == (MOD - 1)) { os << '\n'; }
+    else { os << ' '; }
+  } os << ']'; return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1, T2> &p) { os << "(" << p.first << ", " << p.second << ")"; return os; }
 template<class T> ostream& operator<<(ostream& os, const set<T> &s) {
-  os << '[';
-  if (!s.empty()) {
-    typename set<T>::iterator last = s.end();
-    last--;
-    for (typename set<T>::iterator it = s.begin(); it != s.end(); it++) {
-      os << *it;
-      if (it != last) {
-        os << ' ';
-      }
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// multiset
+  vector<T> v; for (typename set<T>::iterator it = s.begin(); it != s.end(); it++) { v.push_back(*it); };
+  os << v; return os; }
 template<class T> ostream& operator<<(ostream& os, const multiset<T> &ms) {
-  os << '[';
-  for (typename multiset<T>::iterator it = ms.begin(); it != ms.end(); it++) {
-    os << *it;
-    if (it != prev(ms.end())) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// map
+  vector<T> v; for (typename multiset<T>::iterator it = ms.begin(); it != ms.end(); it++) { v.push_back(*it); };
+  os << v; return os; }
 template<class T1, class T2> ostream& operator<<(ostream& os, const map<T1, T2> &m) {
-  os << '[';
-  size_t i = m.size();
+  vector<T2> v; os << '['; size_t i = m.size();
   for (typename map<T1, T2>::const_iterator it = m.begin(); it != m.end(); it++) {
-    os << '{' << it->first << ": " << it->second << '}';
-    if (i > 1) {
-      os << ' ';
-    }
-    i--;
-  }
-  os << ']';
-  return os;
-}
-
-// queue
+    os << '{' << it->first << ": " << it->second << '}'; if (i > 1) { os << ' '; } i--; }
+  os << ']'; return os; }
 template<class T> ostream& operator<<(ostream& os, const queue<T> &q) {
-  os << '[';
-  queue<T> _q(q);
-  while (!_q.empty()) {
-    os << _q.front();
-    _q.pop();
-    if (!_q.empty()) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// priority_queue
+  vector<T> v; queue<T> _q(q); while (!_q.empty()) { v.push_back(_q.front()); _q.pop(); } os << v; return os; }
 template<class T> ostream& operator<<(ostream& os, const priority_queue<T> &q) {
-  os << '[';
-  priority_queue<T> _q(q);
-  while (!_q.empty()) {
-    os << _q.top();
-    _q.pop();
-    if (!_q.empty()) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// priority_queue (greater)
+  vector<T> v; priority_queue<T> _q(q); while (!_q.empty()) { v.push_back(_q.top()); _q.pop(); } os << v; return os; }
 template<class T> ostream& operator<<(ostream& os, const priority_queue<T, vector<T>, greater<T> > &q) {
-  os << '[';
-  priority_queue<T, vector<T>, greater<T> > _q(q);
-  while (!_q.empty()) {
-    os << _q.top();
-    _q.pop();
-    if (!_q.empty()) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// deque
+  vector<T> v; priority_queue<T, vector<T>, greater<T> > _q(q); while (!_q.empty()) { v.push_back(_q.top()); _q.pop(); } os << v; return os; }
 template<class T> ostream& operator<<(ostream& os, const deque<T> &dq) {
-  os << '[';
-  for (auto it = dq.begin(); it != dq.end(); it++) {
-    os << *it;
-    if (it != prev(dq.end())) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
-
-// stack
-#include <vector>
+  vector<T> v; for (auto it = dq.begin(); it != dq.end(); it++) { v.push_back(*it); } os << v; return os; }
 template<class T> ostream& operator<<(ostream& os, const stack<T> &s) {
-  stack<T> _s(s);
-  vector<T> v;
-  while (!_s.empty()) {
-    v.push_back(_s.top());
-    _s.pop();
-  }
-  os << '[';
-  for (int i = v.size() - 1; i >= 0; i--) {
-    os << v[i];
-    if (i != 0) {
-      os << ' ';
-    }
-  }
-  os << ']';
-  return os;
-}
+  vector<T> v; stack<T> _s(s); while (!_s.empty()) { v.push_back(_s.top()); _s.pop(); } reverse(v.begin(), v.end()); os << v; return os; }
 
 // tuple3
 template<class T1, class T2, class T3> ostream& operator<<(ostream& os, const tuple<T1, T2, T3> &t) {
