4d647148c5eec6e6a14d72b38c8d583d7d3a8e8c
kyad <53517611+kyad@users.noreply.github.com>
2022-09-10 16:58:35 +0900

    Update testcase generator

--- testcase/gen.py
+++ testcase/gen.py
@@ -1,4 +1,10 @@
 import random
+
+# N = random.randint(a, b)  # a<=N<=b のNを返す
+
+# alphabet = ''.join([chr(ord('a') + i) for i in range(26)])
+# S = ''.join(random.choices(alphabet, k=len))  # ランダムな文字列
+
 N = 3
 L = 4
 word = [chr(ord('a') + i) for i in range(5)]
@@ -6,5 +12,7 @@ word = [chr(ord('a') + i) for i in range(5)]
 print(N, L)
 
 for n in range(N):
+    # a = random.sample(l, k=k)  # 重複を許さない
+    # a = random.choices(l, k=k)  # 重複を許す
     a = sorted(random.sample(word, 3))  # 重複しないように選ぶ
     print(''.join(a))
