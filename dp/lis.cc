c9c10b862af03fd2eaba92f751087e1c286fd55a
kyad <53517611+kyad@users.noreply.github.com>
2022-10-08 23:01:28 +0900

    Update LIS

--- dp/lis.cc
+++ dp/lis.cc
@@ -1,26 +1,42 @@
-// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_D&lang=ja
+// https://atcoder.jp/contests/arc149/tasks/arc149_b
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main() {
-  const int INF = 1000000001;
-  int n;
-  cin >> n;
+template<class T> int lis(vector<T> X) {
+  const int INF = 1e9 + 2;
+  int n = X.size();
   // L[i] is the minimum last number of subsequence whose length is i
-  vector<int> L(n + 1, INF);
+  vector<T> L(n + 1, INF);
   size_t len = 0;
-  L.at(0) = -1;
+  L.at(0) = -INF;
   for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    vector<int>::iterator it = lower_bound(L.begin(), L.end(), x);
+    vector<int>::iterator it = lower_bound(L.begin(), L.end(), X.at(i));
     size_t pos = distance(L.begin(), it);
     len = max(len, pos);
-    *it = x;
+    *it = X.at(i);
   }
-  cout << len << endl;
+  return (int)len;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  vector<pair<int, int> > P(N);
+  for (int n = 0; n < N; n++) {
+    cin >> P.at(n).first;
+  }
+  for (int n = 0; n < N; n++) {
+    cin >> P.at(n).second;
+  }
+  sort(P.begin(), P.end());
+  vector<int> B(N);
+  for (int n = 0; n < N; n++) {
+    B.at(n) = P.at(n).second;
+  }
+  int ans = N + lis(B);
+  cout << ans << endl;
   return 0;
 }
