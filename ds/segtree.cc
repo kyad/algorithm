de73513f85b88ada67548096ea8ef5f226fccb28
kyad <53517611+kyad@users.noreply.github.com>
2022-11-06 23:34:31 +0900

    Update segtree

--- ds/segtree.cc
+++ ds/segtree.cc
@@ -1,11 +1,15 @@
-// https://atcoder.jp/contests/abc228/tasks/abc228_f
+// https://atcoder.jp/contests/abc276/tasks/abc276_f
 // https://algo-logic.info/segment-tree/
 // https://qiita.com/drken/items/68b8503ad4ffb469624c
 
+#include <cassert>
 #include <functional>
 #include <iostream>
 #include <vector>
 using namespace std;
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
 
 // 抽象化したセグメント木
 template<class X> struct SegTree {
@@ -24,17 +28,21 @@ template<class X> struct SegTree {
     dat.resize(2 * n - 1, ex_);
   }
 
-  // 初期化 O(N) i: 0-indexed
-  void set(int i, X x) { dat[i + n - 1] = x; }  // dat[n-1]~dat[2n-2]に値が入る
-  void set1(int i, X x) { set(i - 1, x); }  // 1-indexed
+  // 初期化 O(N)
+  void set0(int i, X x) { dat[i + n - 1] = x; }  //  i: 0-indexed. dat[n-1]~dat[2n-2]に値が入る
+  void set1(int i, X x) { set0(i - 1, x); }  // i: 1-indexed
   void build() {
     for (int k = n - 2; k >= 0; k--) {
       dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
     }
   }
 
-  // 更新クエリ O(logN) i: 0-indexed
-  void update(int i, X x) {
+  // データ取得 O(1)
+  X get0(int i) { return dat[i + n - 1]; }  // i: 0-indexed
+  X get1(int i) { return get0(i - 1); }  // i: 1-indexed
+
+  // 1点更新クエリ O(logN)
+  void update0(int i, X x) {  // i: 0-indexed
     i += (n - 1);
     dat[i] = x;
     while (i > 0) {  // 最下段から上がる
@@ -42,11 +50,15 @@ template<class X> struct SegTree {
       dat[i] = fx(dat[i * 2 + 1], dat[i * 2 + 2]);
     }
   }
-  void update1(int i, X x) { update(i - 1, x); }  // 1-indexed
+  void update1(int i, X x) { update0(i - 1, x); }  // i: 1-indexed
 
-  // 取得クエリ O(logN) [a, b), a, b: 0-indexed
-  X query(int a, int b) { return query_sub(a, b, 0, 0, n); }
-  X query1(int a, int b) { return query(a - 1, b - 1); }  // 1-indexed
+  // 1点加算クエリ O(logN)
+  void add0(int i, X x) { update0(i, get0(i) + x); }  // i: 0-indexed
+  void add1(int i, X x) { add0(i - 1, x); }  // 1-indexed
+
+  // 範囲取得クエリ O(logN) [a, b)
+  X query0(int a, int b) { return query_sub(a, b, 0, 0, n); }  // a, b: 0-indexed
+  X query1(int a, int b) { return query0(a - 1, b - 1); }  // a, b: 1-indexed
   X query_sub(int a, int b, int k, int l, int r) {
     // k: 現在見ているノードの位置 [l, r): dat[k]が表している区間
     if (r <= a || b <= l) {  // 範囲外
@@ -79,80 +91,84 @@ template<class X> ostream& operator<<(ostream& os, const SegTree<X> &segtree) {
   return os;
 }
 
-// RMQ (Range Minimum Query)
+// 1点更新、区間最小値取得 (RMQ, Range Minimum Query)
 // auto fx = [](int x1, int x2) -> int { return min(x1, x2); };
 // SegTree<int> rmq(n, fx, INF);
 
-// Range Maximum Query
+// 1点更新、区間最大値取得 (Range Maximum Query)
 // auto fx = [](int x1, int x2) -> int { return max(x1, x2); };
 // SegTree<int> rmq(n, fx, -INF);
 
+// 1点更新、区間和
+// auto fx = [](long long x1, long long x2) -> long long { return x1 + x2; };
+// SegTree<long long> seg(M, fx, 0);
+
+// 1点更新、区間のGCD取得
+// const int ex = -1;  // 約数に全ての数を持つ数とする
+// auto fx = [&ex](int x1, int x2) -> int {
+//   if (x1 == ex) {
+//     return x2;
+//   } else if (x2 == ex) {
+//     return x1;
+//   } else {
+//     return __gcd(x1, x2);
+//   }
+// };
+// SegTree<int> gcd(n, fx, ex);
+
 int main() {
-  auto fx = [](long long x1, long long x2) -> long long { return max(x1, x2); };
-  int H, W, h1, w1, h2, w2;
-  cin >> H >> W >> h1 >> w1 >> h2 >> w2;
-  h2 = min(h2, h1);
-  w2 = min(w2, w1);
-  vector<vector<long long> > A(H + 1, vector<long long>(W + 1, 0));  // 1-indexed
-  for (int y = 1; y <= H; y++) {
-    for (int x = 1; x <= W; x++) {
-      cin >> A.at(y).at(x);
-    }
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  const int M = 200000;
+  for (int n = 0; n < N; n++) {
+    cin >> A.at(n);
   }
-  for (int y = 1; y <= H; y++) {
-    for (int x = 1; x <= W; x++) {
-      A[y][x] += A[y][x - 1];
-    }
+#if 1
+  auto fx = [](long long x1, long long x2) -> long long { return x1 + x2; };
+  SegTree<long long> seg(M, fx, 0);
+  SegTree<long long> count(M, fx, 0);
+#else
+  binary_indexed_tree<long long> seg(M);
+  binary_indexed_tree<long long> count(M);
+#endif
+  // dp
+  vector<mint> dp(N, 0);
+  dp[0] = A[0];
+  {
+    int x = A[0];
+    seg.add1(x, x);
+    count.add1(x, 1);
   }
-  for (int x = 1; x <= W; x++) {
-    for (int y = 1; y <= H; y++) {
-      A[y][x] += A[y - 1][x];
-    }
+  for (int i = 1; i < N; i++) {
+    mint iinv = mint(1) / mint(i + 1);
+    mint ex1 = (mint(i) * iinv) * (mint(i) * iinv) * dp[i - 1];
+    dp[i] += ex1;
+    mint ex2 = iinv * iinv * A[i];
+    dp[i] += ex2;
+    // etc
+    int x = A[i];
+#if 1
+    long long sum = seg.query1(x, M + 1);
+#else
+    long long sum = seg.sum1(M) - seg.sum1(x);
+#endif
+    mint ex3 = iinv * iinv * 2 * mint(sum);
+    dp[i] += ex3;
+    
+#if 1
+    long long num = count.query1(1, x);
+#else
+    long long num = count.sum1(x);
+#endif
+    mint ex4 = iinv * iinv * 2 * x * mint(num);
+    dp[i] += ex4;
+    // Update
+    seg.add1(x, x);
+    count.add1(x, 1);
   }
-  // Taka[y][x]: (x, y)から(x+w1, y+h1)までの和 (1-indexed)
-  vector<vector<long long> > Taka(H - h1 + 1 + 1, vector<long long>(W - w1 + 1 + 1, 0));
-  for (int y = 1; y <= H - h1 + 1; y++) {
-    for (int x = 1; x <= W - w1 + 1; x++) {
-      Taka[y][x] = A[y + h1 - 1][x + w1 - 1] - A[y + h1 - 1][x - 1] - A[y - 1][x + w1 - 1] + A[y - 1][x - 1];
-    }
+  for (int n = 0; n < N; n++) {
+    cout << dp[n].val() << '\n';
   }
-  // Aoki[y][x]: (x, y)から(x+w2, y+h2)までの和 (1-indexed)
-  vector<vector<long long> > Aoki(H - h2 + 1 + 1, vector<long long>(W - w2 + 1 + 1, 0));
-  for (int y = 1; y <= H - h2 + 1; y++) {
-    for (int x = 1; x <= W - w2 + 1; x++) {
-      Aoki[y][x] = A[y + h2 - 1][x + w2 - 1] - A[y + h2 - 1][x - 1] - A[y - 1][x + w2 - 1] + A[y - 1][x - 1];
-    }
-  }
-  // P: Aoki[y][x]のx方向の幅w1-w2+1のスライド最小値
-  vector<vector<long long> > P(H - h2 + 1 + 1, vector<long long>(W - w1 + 1 + 1, 0));
-  for (int y = 1; y <= H - h2 + 1; y++) {
-    SegTree<long long> rmq(W - w2 + 1, fx, 0);  // Range Maximum Query
-    for (int x = 1; x <= W - w2 + 1; x++) {
-      rmq.set1(x, Aoki[y][x]);
-    }
-    rmq.build();
-    for (int x = 1; x <= W - w1 + 1; x++) {
-      P[y][x] = rmq.query1(x, x + w1 - w2 + 1);
-    }
-  }
-  // Q: Aoki[y][x]のx方向の幅w1-w2+1、y方向の幅h1-h2+1のスライド最小値
-  vector<vector<long long> > Q(H - h1 + 1 + 1, vector<long long>(W - w1 + 1 + 1, 0));
-  for (int x = 1; x <= W - w1 + 1; x++) {
-    SegTree<long long> rmq(H - h2 + 1, fx, 0);  // Range Maximum Query
-    for (int y = 1; y <= H - h2 + 1; y++) {
-      rmq.set1(y, P[y][x]);
-    }
-    rmq.build();
-    for (int y = 1; y <= H - h1 + 1; y++) {
-      Q[y][x] = rmq.query1(y, y + h1 - h2 + 1);
-    }
-  }
-  long long ans = 0;
-  for (int y = 1; y <= H - h1 + 1; y++) {
-    for (int x = 1; x <= W - w1 + 1; x++) {
-      ans = max(ans, Taka[y][x] - Q[y][x]);
-    }
-  }
-  cout << ans << endl;
   return 0;
 }
