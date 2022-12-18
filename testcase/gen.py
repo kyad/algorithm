e609c58728af51cc9a13df80db1799b56ac73a0b
kyad <53517611+kyad@users.noreply.github.com>
2022-12-18 22:49:42 +0900

    Update testcase

--- testcase/gen.py
+++ testcase/gen.py
@@ -1,18 +1,19 @@
 import random
 
 # N = random.randint(a, b)  # a<=N<=b のNを返す
+# l = list(range(1, N + 1))  # 1, 2, .., N
+# l = [chr(ord('a') + i) for i in range(5)]  # a, b, c, d, e
+# A = random.sample(l, k=k)  # 配列から重複を許さずにk個取る
+# A = sorted(random.sample(word, 3))  # 重複を許さずにk個取ってソートする
+# A = random.choices(l, k=k)  # 配列から重複を許してk個取る
+# r = random.random()  # 0以上1未満の浮動小数点数
 
-# alphabet = ''.join([chr(ord('a') + i) for i in range(26)])
+# l = ''.join([chr(ord('a') + i) for i in range(26)])  # アルファベット全て
 # S = ''.join(random.choices(alphabet, k=len))  # ランダムな文字列
 
-N = 3
-L = 4
-word = [chr(ord('a') + i) for i in range(5)]
+# A = [0] * N
+# for n in range(N):
+#     A[n] = random.randint(1, 10)  # a<=N<=b のNを返す
 
-print(N, L)
-
-for n in range(N):
-    # a = random.sample(l, k=k)  # 重複を許さない
-    # a = random.choices(l, k=k)  # 重複を許す
-    a = sorted(random.sample(word, 3))  # 重複しないように選ぶ
-    print(''.join(a))
+# print(N)
+# print(*A)
