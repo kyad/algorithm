a4974de18324a0459e65b8abc90fb9298e60e39e
kyad <53517611+kyad@users.noreply.github.com>
2023-01-09 12:07:33 +0900

    Update long long sqrt

--- algorithm/long_long_sqrt.cc
+++ algorithm/long_long_sqrt.cc
@@ -1,80 +1,93 @@
-// https://atcoder.jp/contests/abc280/tasks/abc280_d
-
+#include <cassert>
 #include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
 
-// (long long)(sqrt(n)) for n >= 0
-const long long INF = 1000001;  // Please set INF >= max(n) + 1.
-long long long_long_sqrt(long long n) {
-  if (n == 0) {
-    return 0;
-  }    
-  long long left = 0;
-  long long right = INF;
+// sqrt(n) for long long. To avlid overflow of middle * middle, We use sqrt.
+#include <cmath>
+long long long_long_sqrt(long long n) { return round(sqrt(n)); }
+
+// int(sqrt(N))  O(log(N))
+int int_sqrt(int N) {
+  int left = 0;
+  int right = N;
   while (right - left > 1) {
-    long long mid = (left + right) / 2;
-    long long y = (long long)mid * mid - n;
-    if (y <= 0) {
-      left = mid;
+    int middle = (left + right) / 2;
+    if (middle * middle >= N) {
+      right = middle;
     } else {
-      right = mid;
+      left = middle;
     }
   }
-  return left;
+  if (right * right >= N) {
+    return right;
+  } else {
+    return left;
+  }
 }
 
-// O(sqrt(n))
-vector<long long> prime_factorization(long long n) {
-  long long max = long_long_sqrt(n);
-  //print(n, max);
-  vector<long long> ans;
-  for (long long m = 2; m <= max; m++) {
-    while (n % m == 0) {
-      n /= m;
-      ans.push_back(m);
+// Sieve of Eratosthenes  O(N * log log (N))
+vector<bool> eratosthenes(int N) {
+  vector<bool> is_prime(N + 1, true);
+  is_prime.at(0) = false;
+  if (N > 0) {
+    is_prime.at(1) = false;
+  }
+  for (int k = 2; k < int_sqrt(N); k++) {
+    for (int i = 2 * k; i <= N; i+=k) {
+      is_prime.at(i) = false;
     }
   }
-  if (n != 1) {
-    ans.push_back(n);
-  }
-  return ans;
-}
-
-long long solve(long long p, int cnt) {
-  long long n = p;
-  while (true) {
-    long long now = n;
-    while (now % p == 0) {
-      now /= p;
-      cnt--;
-      if (cnt == 0) {
-        break;
-      }
-    }
-    if (cnt == 0) {
-      break;
-    }
-    n += p;
-  }
-  return n;
+  return is_prime;
 }
 
 int main() {
-  long long K;
-  cin >> K;
-  vector<long long> ps = prime_factorization(K);
-  map<long long, int> mp;
-  for (size_t i = 0; i < ps.size(); i++) {
-    long long p = ps[i];
-    mp[p]++;
+  int M = 3000000;
+  vector<bool> is_prime = eratosthenes(M);
+  vector<long long> primes;
+  for (int m = 2; m <= M; m++) {
+    if (is_prime[m]) {
+      primes.push_back(m);
+    }
   }
-  long long ans = 0;
-  for (auto [p, cnt] : mp) {
-    long long now = solve(p, cnt);
-    ans = max(ans, now);
+  int T;
+  cin >> T;
+  for (int t = 0; t < T; t++) {
+    // testcase
+    long long N;
+    cin >> N;
+    bool ok = false;
+    // Fix p
+    for (long long p : primes) {
+      if (p * p > N) {
+        break;
+      }
+      long long q = N / p / p;
+      if (p * p * q != N) {
+        continue;
+      }
+      cout << p << ' ' << q << '\n';
+      ok = true;
+      break;
+    }
+    if (ok) {
+      continue;
+    }
+    // Fix q
+    for (long long q : primes) {
+      long long p2 = N / q;
+      long long p = long_long_sqrt(p2);
+      if (p * p * q != N) {
+        continue;
+      }
+      cout << p << ' ' << q << '\n';
+      ok = true;
+      break;
+    }
+    if (ok) {
+      continue;
+    }
+    assert(false);
   }
-  cout << ans << endl;
   return 0;
 }
