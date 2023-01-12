36040e9ab5e3a8b91f8d8e75ae1e209a33bc0045
kyad <53517611+kyad@users.noreply.github.com>
2023-01-13 00:27:03 +0900

    Update Ford-Fulkerson algorithm

--- graph/ford-fulkerson.cc
+++ graph/ford-fulkerson.cc
@@ -1,4 +1,4 @@
-// https://atcoder.jp/contests/abc010/tasks/abc010_4
+// https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
 // https://drken1215.hatenablog.com/entry/2021/08/04/155100
 // https://github.com/drken1215/book_algorithm_solution/blob/master/codes/chap16/code_16_1.cpp
 
@@ -22,7 +22,7 @@ struct Graph {
   size_t size() const {
     return list.size();
   }
-  vector<Edge> &operator [] (int i) {
+  vector<Edge> &operator [] (int i) {  // 0-indexed
     return list[i];
   }
   Edge& redge(const Edge &e) {
@@ -32,7 +32,7 @@ struct Graph {
     e.cap -= f;
     redge(e).cap += f;
   }
-  void addedge(int from, int to, int cap) {
+  void addedge(int from, int to, int cap) {  // from and to are 0-indexed
     int fromrev = (int)list[from].size();
     int torev = (int)list[to].size();
     list[from].push_back(Edge(torev, from, to, cap));
@@ -89,23 +89,17 @@ struct FordFulkerson {
   }
 };
 
+// Example
 int main() {
-  int N, G, E;
-  cin >> N >> G >> E;
-  Graph graph(N + 1);
-  for (int g = 0; g < G; g++) {
-    int p;
-    cin >> p;
-    graph.addedge(p, N, 1);
-    graph.addedge(N, p, 1);
-  }
-  for (int e = 0; e < E; e++) {
-    int a, b;
-    cin >> a >> b;
-    graph.addedge(a, b, 1);
-    graph.addedge(b, a, 1);
+  int N, M;
+  cin >> N >> M;
+  Graph graph(N);
+  for (int m = 0; m < M; m++) {
+    int U, V, C;
+    cin >> U >> V >> C;
+    graph.addedge(U, V, C);
   }
   FordFulkerson ff;
-  cout << ff.solve(graph, 0, N) << endl;
+  cout << ff.solve(graph, 0, N - 1) << endl;
   return 0;
 }
