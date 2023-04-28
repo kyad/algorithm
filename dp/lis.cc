6cc0bec81bfac73daeebceab7a1eab6f9ff2bfef
kyad <53517611+kyad@users.noreply.github.com>
2023-04-29 00:27:56 +0900

    Update LIS

--- dp/lis.cc
+++ dp/lis.cc
@@ -1,42 +1,59 @@
-// https://atcoder.jp/contests/arc149/tasks/arc149_b
+// https://atcoder.jp/contests/typical90/tasks/typical90_bh
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-template<class T> int lis(vector<T> X) {
-  const int INF = 1e9 + 2;
-  int n = X.size();
-  // L[i] is the minimum last number of subsequence whose length is i
-  vector<T> L(n + 1, INF);
-  size_t len = 0;
-  L.at(0) = -INF;
-  for (int i = 0; i < n; i++) {
-    vector<int>::iterator it = lower_bound(L.begin(), L.end(), X.at(i));
-    size_t pos = distance(L.begin(), it);
-    len = max(len, pos);
+/**
+ * 最長増加部分列 (Longest Increasing Subsequence, LIS)
+ * 計算量 O(NlogN)
+ */
+template<class T> pair<vector<int>, vector<T> > lis(const vector<T>& X) {
+  const int INF = 1e9+1;
+  int N = X.size();
+  // dp[i]: 長さiの単調増加列の末尾の数字の最小値 (0<=i<=N)
+  vector<T> dp(N + 1, INF);
+  dp[0] = -INF;
+  // len[i]: iまで見た時の最大の単調増加列の長さ (0<=i<N)
+  vector<int> len(N, 0);
+  // value[i]: iまで見た時の最大の単調増加列の末尾の数字の最小値 (0<=i<N)
+  vector<T> value(N, 0);
+  for (int i = 0; i < N; i++) {
+    vector<int>::iterator it = lower_bound(dp.begin(), dp.end(), X.at(i));
+    size_t pos = it - dp.begin();
+    if (i == 0) {
+      len.at(0) = (int)pos;
+    } else {
+      len.at(i) = max((int)pos, len.at(i - 1));
+    }
     *it = X.at(i);
+    value.at(i) = dp.at(len.at(i));
   }
-  return (int)len;
+  return make_pair(len, value);
 }
 
 int main() {
   int N;
   cin >> N;
-  vector<pair<int, int> > P(N);
+  vector<int> A(N);
   for (int n = 0; n < N; n++) {
-    cin >> P.at(n).first;
+    cin >> A.at(n);
   }
-  for (int n = 0; n < N; n++) {
-    cin >> P.at(n).second;
+  auto lis0 = lis(A);
+  reverse(A.begin(), A.end());
+  auto lis1 = lis(A);
+  int ans = 0;
+  for (int k = 0; k < N - 1; k++) {
+    int now = lis0.first[k] + lis1.first[N - k - 2];
+    if (lis0.second[k] == lis1.second[k]) {
+      now--;
+    }
+    ans = max(ans, now);
   }
-  sort(P.begin(), P.end());
-  vector<int> B(N);
-  for (int n = 0; n < N; n++) {
-    B.at(n) = P.at(n).second;
-  }
-  int ans = N + lis(B);
+  ans = max(ans, lis0.first[N - 1]);
+  ans = max(ans, lis1.first[N - 1]);
   cout << ans << endl;
   return 0;
 }
