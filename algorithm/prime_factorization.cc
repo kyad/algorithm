e6f180a1869148fec18e21593bfd1b3a82a06941
kyad <53517611+kyad@users.noreply.github.com>
2023-07-14 00:49:39 +0900

    Update prime factorization

--- algorithm/prime_factorization.cc
+++ algorithm/prime_factorization.cc
@@ -1,32 +1,42 @@
-// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
+// https://atcoder.jp/contests/typical90/tasks/typical90_bw
 
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-// O(sqrt(n))
-vector<int> prime_factorization(int n) {
-  vector<int> ans;
-  for (long long m = 2; m * m <= ((long long)n); m++) {  // Care for overflow
-    while (n % m == 0) {
-      n /= m;
+/**
+ * 素因数分解して結果を重複も込めて小さい順に返す。
+ * 計算量: O(sqrt(N))
+ * 制約: Tはint, unsigned int, long long, unsigned long longのいずれか
+ * \param N 素因数分解する数
+ * \return vector<T> 素因数の配列。重複も込める。小さい順。1は含めない
+ */
+template<typename T> vector<T> prime_factorization(T N) {
+  vector<T> ans;
+  for (long long m = 2; m * m <= (long long)N; m++) {  // Care for overflow
+    while (N % m == 0) {
+      N /= m;
       ans.push_back(m);
     }
   }
-  if (n != 1) {
-    ans.push_back(n);
+  if (N != 1) {
+    ans.push_back(N);
   }
   return ans;
 }
 
 int main() {
-  int n;
-  cin >> n;
-  cout << n << ":";
-  vector<int> ans = prime_factorization(n);
-  for (size_t i = 0; i < ans.size(); i++) {
-    cout << ' ' << ans.at(i);
+  long long N;
+  cin >> N;
+  vector<long long> ps = prime_factorization(N);
+  int x = ps.size() - 1;
+  int ans = 0;
+  while (x > 0) {
+    x >>= 1;
+    ans++;
   }
-  cout << endl;
+  cout << ans << endl;
   return 0;
 }
