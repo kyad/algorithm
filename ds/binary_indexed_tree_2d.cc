cdd82a7228b02dac80b189c6d5daa0d441115640
kyad <53517611+kyad@users.noreply.github.com>
2023-08-18 23:23:01 +0900

    Update binary indexed tree 2d

--- ds/binary_indexed_tree_2d.cc
+++ ds/binary_indexed_tree_2d.cc
@@ -1,36 +1,45 @@
-// https://atcoder.jp/contests/joi2011ho/tasks/joi2011ho1
-// https://atcoder.jp/contests/abc221/editorial/2718
-// https://algo-logic.info/binary-indexed-tree/
+// https://atcoder.jp/contests/abc311/tasks/abc311_f
 
+#include <algorithm>
+#include <cassert>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
+// modint
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
+ostream& operator<<(ostream& os, const mint &v) {
+  os << v.val();
+  return os;
+}
 
-struct binary_indexed_tree_2d {
+template<class T> struct binary_indexed_tree_2d {
   int H, W;
-  vector<vector<int> > bit;
+  vector<vector<T> > bit;
   binary_indexed_tree_2d() {}
   binary_indexed_tree_2d(int h, int w) : H(h), W(w) {
-    bit.assign(H + 1, vector<int>(W + 1, 0));
+    bit.assign(H + 1, vector<T>(W + 1, 0));
   }
   void resize(int h, int w) {
     H = h;
     W = w;
-    bit.assign(H + 1, vector<int>(W + 1, 0));
+    bit.assign(H + 1, vector<T>(W + 1, 0));
   }
-  // Add a to bit[y][x], O(logH * logW)
-  // y, x: 1-indexed
-  void add(int y, int x, int a) {
+  // Add a to bit[y][x], O(logH * logW). y, x: 1-indexed
+  void add1(int y, int x, T a) {
     for (int Y = y; Y <= H; Y += (Y & -Y)) {
       for (int X = x; X <= W; X += (X & -X)) {
         bit[Y][X] += a;
       }
     }
   }
-  // Returns sum of bit[Y][X] for 1 <= Y <= y, 1 <= X <= x, O(logH * logW)
-  // y, x: 1-indexed
-  int sum(int y, int x) {
-    int ret = 0;
+  // Add a to bit[y][x], O(logH * logW). y, x: 0-indexed
+  void add0(int y, int x, T a) { add1(y + 1, x + 1, a); }
+  // Returns sum of bit[Y][X] for 1 <= Y <= y, 1 <= X <= x, O(logH * logW). y, x: 1-indexed
+  T sum1(int y, int x) {
+    T ret = 0;
     for (int Y = y; Y > 0; Y -= (Y & -Y)) {
       for (int X = x; X > 0; X -= (X & -X)) {
         ret += bit[Y][X];
@@ -38,47 +47,81 @@ struct binary_indexed_tree_2d {
     }
     return ret;
   }
-  // Returns sum of bit[Y][X] for y0 <= Y <= y1, x0 <= X <= x1, O(logH * logW)
-  // y0, y1, x0, x1: 1-indexed
-  int sum_range(int y0, int x0, int y1, int x1) {
-    return sum(y1, x1) - sum(y1, x0 - 1) - sum(y0 - 1, x1) + sum(y0 - 1, x0 - 1);
+  // Returns sum of bit[Y][X] for 0 <= Y <= y, 0 <= X <= x, O(logH * logW). y, x: 0-indexed
+  // 半開区間の場合は[0, y)*[0, x)の少なくとも一方の軸が空集合の場合にも使え、0を返す。
+  T sum0(int y, int x) { return sum1(y + 1, x + 1); }
+  T sum0_open(int y, int x) { return (y == 0 || x == 0) ? 0 : sum0(y - 1, x - 1); }
+  // Returns sum of bit[Y][X] for y0 <= Y <= y1, x0 <= X <= x1, O(logH * logW). y0, y1, x0, x1: 1-indexed
+  T sum1(int y0, int x0, int y1, int x1) {
+    return sum1(y1, x1) - sum1(y1, x0 - 1) - sum1(y0 - 1, x1) + sum1(y0 - 1, x0 - 1);
   }
+  // Returns sum of bit[Y][X] for y0 <= Y <= y1, x0 <= X <= x1, O(logH * logW). y0, y1, x0, x1: 0-indexed
+  // 半開区間の場合は[y0, y1)*[x0, x1)の少なくとも一方の軸が空集合の場合にも使え、0を返す。
+  T sum0(int y0, int x0, int y1, int x1) { return sum1(y0 + 1, x0 + 1, y1 + 1, x1 + 1); }
+  T sum0_open(int y0, int x0, int y1, int x1) { return (y0 == y1 || x0 == x1) ? 0 : sum0(y0, x0, y1 - 1, x1 - 1); }
 };
+// Example
+// binary_indexed_tree_2d<int> bit(N, M);
+// 1点(y, x)の取得：sum0(y, x, y, x)
 
 int main() {
-  int M, N;;
-  cin >> M >> N;
-  int K;
-  cin >> K;
-  binary_indexed_tree_2d bit2d_j(M, N);
-  binary_indexed_tree_2d bit2d_o(M, N);
-  binary_indexed_tree_2d bit2d_i(M, N);
-  for (int y = 0; y < M; y++) {
-    string line;
-    cin >> line;
-    for (int x = 0; x < N; x++) {
-      switch (line[x]) {
-      case 'J': {
-        bit2d_j.add(y + 1, x + 1, 1);
-        break;
-      }
-      case 'O': {
-        bit2d_o.add(y + 1, x + 1, 1);
-        break;
-      }
-      case 'I': {
-        bit2d_i.add(y + 1, x + 1, 1);
-        break;
-      }
+  int N, M;
+  cin >> N >> M;
+  vector<string> S(N);
+  for (int n = 0; n < N; n++) {
+    cin >> S.at(n);
+  }
+  // bfs
+  queue<pair<int, int> > todo;
+  vector<vector<int> > dist(N, vector<int>(M, -1));
+  for (int y = 0; y < N; y++) {
+    for (int x = 0; x < M; x++) {
+      if (S[y][x] == '#') {
+        dist[y][x] = 0;
+        todo.push(make_pair(y, x));
       }
     }
   }
-  for (int k = 0; k < K; k++) {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    cout << bit2d_j.sum_range(a, b, c, d) << ' '
-         << bit2d_o.sum_range(a, b, c, d) << ' '
-         << bit2d_i.sum_range(a, b, c, d) << endl;
+  while (!todo.empty()) {
+    const pair<int, int> u = todo.front();
+    todo.pop();
+    const int y = u.first;
+    const int x = u.second;
+    for (int dx = 0; dx <= 1; dx++) {
+      const int ny = y + 1;
+      const int nx = x + dx;
+      if (ny >= N || nx >= M) {
+        continue;
+      }
+      if (dist[ny][nx] < 0) {
+        dist[ny][nx] = dist[y][x] + 1;
+        todo.push(make_pair(ny, nx));
+      }
+    }
   }
+
+  vector<vector<mint> > dp(N, vector<mint>(M, 0));
+  binary_indexed_tree_2d<mint> bit(N, M);
+  
+  for (int x = M - 1; x >= 0; x--) {
+    for (int y = 0; y < N; y++) {
+      if (dist[y][x] >= 0) {
+        break;
+      }
+      if (y + 1 < N && x + 1 < M && dist[y + 1][x + 1] < 0) {  // 右下が白の時
+        dp[y][x] = bit.sum0_open(0, x + 1, y + 2, x + 2);
+      } else {  // 右下が黒の時
+        dp[y][x] = bit.sum0_open(0, x + 1, N, M) + 1;
+      }
+      bit.add0(y, x, dp[y][x]);
+    }
+  }
+  mint ans = 1;  // 塗らない
+  for (int y = 0; y < N; y++) {
+    for (int x = 0; x < M; x++) {
+      ans += dp[y][x];
+    }
+  }
+  cout << ans.val() << endl;
   return 0;
 }
