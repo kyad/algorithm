ffb352bdeb92ab4647a66cfc0724428672bb2dec
kyad <53517611+kyad@users.noreply.github.com>
2025-01-02 00:03:15 +0900

    Update next combination

--- algorithm/next_combination.py
+++ algorithm/next_combination.py
@@ -1,9 +1,36 @@
-# https://atcoder.jp/contests/abc263/tasks/abc263_c
+# https://atcoder.jp/contests/abc386/tasks/abc386_e
+# https://atcoder.jp/contests/abc386/editorial/11697
 
+# import pypyjit
 import itertools
+import sys
 
-N, M = map(int, input().split())
+# pypyjit.set_param("max_unroll_recursion=-1")
+sys.set_int_max_str_digits(0)
+sys.setrecursionlimit(10**6)
 
-l = [ i + 1 for i in range(M) ]
-for a in itertools.combinations(l, N):
-    print(*a, sep=' ')
+
+def main():
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    ans = 0
+    if K <= N - K:  # Kが大きい場合は時間がかかるのでN-Kとして処理するために場合分けする
+        for x in itertools.combinations(A, K):
+            now = 0
+            for a in x:
+                now ^= a
+            ans = max(ans, now)
+    else:
+        all_sum = 0
+        for a in A:
+            all_sum ^= a
+        for x in itertools.combinations(A, N - K):
+            now = all_sum
+            for a in x:
+                now ^= a
+            ans = max(ans, now)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
